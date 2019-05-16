e7e801ef24bbfaee7825b225e82e0b7968f18f1e
Hayden <ghhoky43@gmail.com>
2019-05-16 08:15:36 -0600

    first commit

---
+++ Coding_Challenge_#2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coding_Challenge_#2
+//
+//  Created by Hayden Hoki on 5/16/19.
+//  Copyright © 2019 Hayden Hoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
